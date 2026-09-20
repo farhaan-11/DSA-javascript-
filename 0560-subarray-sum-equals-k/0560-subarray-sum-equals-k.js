f8a2ff4beb143c0e8f0feb17bf394be0a9508fd7
farhan ansari <145009116+farhaan-11@users.noreply.github.com>
2026-09-20 13:47:04 +0530

    Time: 17 ms (66.94%), Space: 66.1 MB (14.26%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -4,20 +4,38 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    // brute force approach---
+  
     let count=0;
+  // brute force approach---
+    // for(let i=0; i<nums.length; i++){
+
+    //     let sum=0
+    //     for(let j=i; j<nums.length; j++){
+
+    //         sum+=nums[j]
+    //         if(sum===k) count++
+
+    //     }
+    // }
+
+    //  optimized ----
+
+    let map=new Map()
+    let sum=0;
+    map.set(0,1)
+    // console.log(map)
 
     for(let i=0; i<nums.length; i++){
+        sum+=nums[i]
+        let rem=sum - k
 
-        let sum=0
-        for(let j=i; j<nums.length; j++){
+        if(map.has(rem)){
+            count+=map.get(rem)
+        } 
 
-            sum+=nums[j]
-            if(sum===k) count++
-
-
-
-        }
+        map.set(sum, (map.get(sum) || 0) + 1)
     }
+     console.log(map)
+
     return count;
 };
