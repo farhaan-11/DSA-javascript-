1db2c73060085dc59429babdabbe0565d78b87fc
farhan ansari <145009116+farhaan-11@users.noreply.github.com>
2026-09-24 08:41:09 +0530

    Time: 14 ms (8.75%), Space: 61.1 MB (21.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -5,18 +5,26 @@
 
 
 
-
-var isPalindrome = function(s = ':aa/') {
+var isPalindrome = function(s = '.....') {
+    console.log('string length',s.length)
     
-const str = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+// const str = s.toLowerCase().replace(/[^a-z0-9]/g, '');
 
     let i = 0;
-    let j = str.length - 1;
+    let j = s.length - 1;
 
     while (i < j) {
-        if (str[i] !== str[j]) {
-            return false;
-        }
+      
+        while (i<j && !((s[i].toLowerCase().charCodeAt(0) >= 97 && s[i].toLowerCase().charCodeAt(0) <= 122) || (s[i].toLowerCase().charCodeAt(0) >= 48 && s[i].toLowerCase().charCodeAt(0) <= 57))) {
+    // neither lowercase letter nor number
+    i++
+}
+
+  while (j>i && !((s[j].toLowerCase().charCodeAt(0) >= 97 && s[j].toLowerCase().charCodeAt(0) <= 122) || (s[j].toLowerCase().charCodeAt(0) >= 48 && s[j].toLowerCase().charCodeAt(0) <= 57))) {
+    // neither lowercase letter nor number
+    j--
+}
+if(j>i && s[i].toLowerCase()!==s[j].toLowerCase()) return false
 
         i++;
         j--;
